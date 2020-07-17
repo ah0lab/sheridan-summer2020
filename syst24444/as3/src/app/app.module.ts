4c1b12e234758caaf0b44d096e137949901dc72a
benjm-ma <bahola@me.com>
2020-07-16 22:30:04 -0400

    Added Slide Component

--- syst24444/as3/src/app/app.module.ts
+++ syst24444/as3/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SliderComponent } from './slider/slider.component';
+import { SlideComponent } from './slide/slide.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    SliderComponent
+    SliderComponent,
+    SlideComponent
   ],
   imports: [
     BrowserModule
