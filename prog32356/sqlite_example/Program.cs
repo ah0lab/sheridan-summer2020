73f93224d1dde34147c99b54846d816c9ef7fff3
benjm-ma <bahola@me.com>
2020-07-19 09:42:23 -0400

    basic ado example using sqlite

---
+++ prog32356/sqlite_example/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.Sqlite;
+using System;
+
+namespace sqlite_example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var conn = new SqliteConnection ("Data Source=db/chinook.db")) {
+                conn.Open ();
+
+                var command = conn.CreateCommand ();
+                command.CommandText = 
+                    @"
+                        SELECT title
+                            FROM albums;
+                    ";
+
+                using (var reader = command.ExecuteReader ()) {
+                    while (reader.Read ())
+                    {
+                        var title = reader.GetString (0);
+                        Console.WriteLine ($"{title}\n");
+                    }
+                }
+            }
+        }
+    }
+}
