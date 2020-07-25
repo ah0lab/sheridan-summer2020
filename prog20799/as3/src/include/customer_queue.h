bc23bbcb2e6fb393ca1c478143242eaa6a4712ec
benjm-ma <bahola@me.com>
2020-07-25 15:41:47 -0400

    Additional changes made

--- prog20799/as3/src/include/customer_queue.h
+++ prog20799/as3/src/include/customer_queue.h
@@ -2,4 +2,8 @@
 typedef CustomerRecord QueueData;
 #include "queue.h"
 
+#define add_record(queue, record) enqueue(queue, record)
+#define remove_record(queue) dequeue(queue)
+#define init_record_queue() initQueue()
+
 
