c5d5d091e0559741994a435b09288b0d89d10679
benjm-ma <bahola@me.com>
2020-08-02 15:08:27 -0400

    Completed good portion of coding first part

---
+++ prog32356/assignment_2/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+
+namespace Assignment
+{
+    public static class DBAconnection
+    {
+        private static string connectionString = "User Id=S8_AHOLAB; Data Source=boba.fast.sheridanc.on.ca:1521/ACES.FAST.SHERIDANC.ON.CA;"; 
+
+        public static void Connect() {
+            using (OracleConnection con = new OracleConnection (connectionString)) {
+                using (OracleCommand cmd = con.CreateCommand ()) {
+                    try {
+                        con.Open();
+                        cmd.BindByName = true;
+
+                        cmd.CommandText = "select ProductID, ProductName from products;";
+
+                        OracleDataReader reader = cmd.ExecuteReader ();
+                        while (reader.Read ()) {
+                            Console.WriteLine ($"Returned Data : {reader.GetString(0)}\n");
+                        }
+                        reader.Dispose ();
+                    } catch (Exception ex) {
+                        Console.WriteLine (ex.Message);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ prog32356/assignment_2/test/controller/Employee.controller.test.cs
@@ -0,0 +1,59 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using Assignment.Model;
+using Assignment.Controller;
+
+namespace Assignment.Test
+{
+    public class EmployeeControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Should_RetrieveAllEmployees()
+        {
+            List<Employee> employees = EmployeeController.GetEmployees ();
+            foreach (Employee e in employees) {
+                Console.WriteLine (e);
+            }
+        }
+
+        [Test]
+        public void Should_RetrieveEmployeeByFirstname ()
+        {
+            List<Employee> retrievedEmployees = EmployeeController.GetEmployeeByFirstName ("Davolio");
+            foreach (Employee e in retrievedEmployees) {
+                Console.WriteLine (e);
+            }
+        }
+
+        [Test]
+        public void Should_CreateANewEmployee()
+        {
+            EmployeeController.AddEmployee (new Employee (
+                id              : 10,
+                firstname       : "Ben",
+                lastname        : "Ahola",
+                title           : "student",
+                titleofcourtesy : "mr.",
+                birthdate       : DateTime.Parse("06/03/1990"),
+                hiredate        : DateTime.Today,
+                address         : new Address (
+                    street      : "123 Fake st",
+                    city        : "Faketown",
+                    region      : "ON",
+                    postalcode  : "L6W5C2",
+                    country     : "Canada"),
+                homephone       : "911",
+                extension       : "3543",
+                photo           : "handsom.gif",
+                notes           : "Just a student programming a program",
+                reportsto       : 3)
+            );
+        }
+    }
+}
