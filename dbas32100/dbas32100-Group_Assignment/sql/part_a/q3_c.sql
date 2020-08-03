f8f2f9bc6071e31ef0700b012017761b389014cd
benjm-ma <bahola@me.com>
2020-08-02 20:22:47 -0400

    Part A : Q3 - C & Q3 - D Completed & Tested. Part B : Q2 Completed & Tested

--- dbas32100/dbas32100-Group_Assignment/sql/part_a/q3_c.sql
+++ dbas32100/dbas32100-Group_Assignment/sql/part_a/q3_c.sql
@@ -4,5 +4,12 @@
 --  customer and the movies he/she 
 --  is renting.
 --
--- Contributor : 
+-- Contributor : Benjamin Ahola
 ----------------------------------
+SELECT c.CustomerID, FirstName, LastName, PhoneNumber, 
+       Birthdate, DriversLicenseNumber, Status, CreditCardNumber,
+       t.TITLE
+    FROM Customer_T c JOIN RENTAL_T r
+        ON c.CustomerID = r.CustomerID 
+        JOIN TITLES_T t
+        ON t.TapeID = r.TapeID;
