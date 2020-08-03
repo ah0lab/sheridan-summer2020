8fda1a54f63bc542ba158b5b8dc49532b565a7ae
benjm-ma <bahola@me.com>
2020-08-03 10:49:09 -0400

    Made fixes to deletion scripts (Thanks Joshua\!)

--- dbas32100/dbas32100-Group_Assignment/sql/part_a/q2_delete.sql
+++ dbas32100/dbas32100-Group_Assignment/sql/part_a/q2_delete.sql
@@ -8,26 +8,26 @@
 ACCEPT p_CategoryID PROMPT 'Enter the category ID'
 BEGIN
     DELETE FROM Category_T 
-        WHERE CategoryID = p_CategoryID;
+        WHERE CategoryID = &p_CategoryID;
 END;
 
 --Customer Table
 ACCEPT p_CustomerID PROMPT 'Enter the customer ID'
 BEGIN
     DELETE FROM Customer_T
-        WHERE CustomerID = p_CustomerID;
+        WHERE CustomerID = &p_CustomerID;
 END;
 
 --Titles Table
 ACCEPT p_TapeID PROMPT 'Enter the tape ID'
 BEGIN
     DELETE FROM Titles_T
-        WHERE TapeID = p_TapeID;
+        WHERE TapeID = &p_TapeID;
 END;
 
 --Rental Table
 ACCEPT p_RentalID PROMPT 'Enter the rental ID'
 BEGIN
     DELETE FROM Rentals_T
-        WHERE RentalID = p_RentalID;
+        WHERE RentalID = &p_RentalID;
 END; 
