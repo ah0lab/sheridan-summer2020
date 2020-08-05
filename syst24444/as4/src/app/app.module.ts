5b94a33b889198f261b39fd5b1a8dec8ed7c2a1b
benjm-ma <bahola@me.com>
2020-08-04 22:21:10 -0400

    Completed assignment 4

--- syst24444/as4/src/app/app.module.ts
+++ syst24444/as4/src/app/app.module.ts
@@ -4,17 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WeatherComponent } from './weather/weather.component';
-import { WeatherforumComponent } from './weatherforum/weatherforum.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     WeatherComponent,
-    WeatherforumComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- syst24444/as4/src/app/weather/weather.component.ts
+++ syst24444/as4/src/app/weather/weather.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-weather',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  public apiKey : string = "dd76821cd1852f61fc0ecbf17b3dca50";
+  public city : FormControl = new FormControl ('');
+
+  public temp : number = 0;
+  public lat : number = 0;
+  public long : number = 0;
+
+  public retrieveData() 
+  {
+    let obs = 
+      this.http.get<any> (`http://api.openweathermap.org/data/2.5/weather?q=${this.city.value}&appid=${this.apiKey}`);
+
+    obs.subscribe ((response) => {
+      console.log (response);
+      this.long = response.coord.lon;
+      this.lat  = response.coord.lat;
+      this.temp = response.main.temp;
+    });
+  }
 
   ngOnInit(): void {
+    this.city.setValue('Brampton');
   }
 
 }
