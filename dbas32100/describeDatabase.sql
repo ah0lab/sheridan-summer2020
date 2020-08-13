a549132547b3e523be57058da20202b9f81c8e1b
benjm-ma <bahola@me.com>
2020-08-13 13:03:52 -0400

    Added another table description query

--- dbas32100/describeDatabase.sql
+++ dbas32100/describeDatabase.sql
@@ -2,9 +2,12 @@ SELECT owner, TABLE_NAME
 FROM all_tables 
 WHERE owner IN ('SI', 'HR');
 
-DESCRIBE SI.Cars;
+DESCRIBE SI.Car;
 DESCRIBE SI.Customer;
 DESCRIBE SI.SALEINV;
 DESCRIBE hr.DEPARTMENTS;
+DESCRIBE hr.EMPLOYEES;
+
+SELECT Department_ID, Department_Name FROM hr.Departments;
 
 SELECT * FROM SI.SALEINV;
